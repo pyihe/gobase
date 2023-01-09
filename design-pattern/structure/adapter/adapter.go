95fd8a101f039462dd0c701e30f8e1b1e715a291
pyihe <785131182@qq.com>
2023-01-09 16:22:13 +0800

    refactor: structure pattern

--- design-pattern/structure/adapter/adapter.go
+++ design-pattern/structure/adapter/adapter.go
@@ -1,31 +1,49 @@
-package adapter
+package main
+
+import "fmt"
 
 /*
-	Singer接口: 可以唱歌
-	Jumper接口: 可以跳跃
-
-	现在我想跳着唱歌，或者不管唱歌或者跳跃，每次都只需要通过一次调用就能达到效果
-	这是可以用适配器在中间进行适配
+	两个需要没有关联但需要交互的类型之间通过一个适配器进行交互
+	比如国内外的插座，需要一个转接器才能使电器正常工作
 */
 
-type Singer interface {
-	Sing()
+// Adapter 适配器
+type Adapter interface {
+	Adapt()
 }
 
-type Jumper interface {
-	Jump()
+type adapter struct {
+	*AmericanStandard
 }
 
-type Adapter struct {
-	Singer
-	Jumper
+func newAdapter(a *AmericanStandard) Adapter {
+	return &adapter{a}
 }
 
-func (a *Adapter) Act(sing, jump bool) {
-	if sing {
-		a.Sing()
+func (ad *adapter) Adapt() {
+	ad.Work()
+}
+
+type AmericanStandard struct{}
+
+func (a *AmericanStandard) Work() {
+	fmt.Println("美标可以正常工作了")
+}
+
+type NationalStandard struct {
+}
+
+func (n *NationalStandard) Work(ad ...Adapter) {
+	fmt.Println("国标可以工作了")
+	if len(ad) > 0 {
+		ad[0].Adapt()
 	}
-	if jump {
-		a.Jump()
-	}
+}
+
+func main() {
+	// 国标和美标必须同时工作电器才能正常使用
+	ns := &NationalStandard{}
+	as := &AmericanStandard{}
+	adapter := newAdapter(as)
+	ns.Work(adapter)
 }
